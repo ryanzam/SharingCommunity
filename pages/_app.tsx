3910459af416b5bdbe5710cac0864593319074af
Mahamad Shah <qaiumshah@yahoo.com>
2023-05-07 13:30:43 +0300

    boostrap added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,34 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./custom.css"
-import NavBar from "../components/Nav";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { CookiesProvider } from 'react-cookie';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function App({ Component, pageProps: { session, ...pageProps} } : any)
-{
-    return <>
-            <CookiesProvider>
-                <NavBar {...pageProps}/>
-                <Component {...pageProps} />
-            </CookiesProvider>
-            <ToastContainer position="bottom-left"
-                        autoClose={5000}
-                        hideProgressBar
-                        newestOnTop={false}
-                        closeOnClick
-                        rtl={false}
-                        pauseOnFocusLoss
-                        draggable
-                        pauseOnHover
-                        theme="dark" />
+import "./custom.css";
+import NavBar from "../components/Nav";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { CookiesProvider } from "react-cookie";
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: any) {
+  return (
+    <>
+      <CookiesProvider>
+        <NavBar {...pageProps} />
+        <Component {...pageProps} />
+      </CookiesProvider>
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </>
+  );
 }
